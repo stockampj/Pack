32c01b54815e2660f4529b8b4fb0e0a7faee31fc
joel stockamp <joel@>
2019-10-09 09:44:21 -0700

    add a PackItem class and checkoff method for the packed property

--- Pack/Models/PackList.cs
+++ Pack/Models/PackList.cs
@@ -3,8 +3,36 @@ using System;
 
 namespace Pack.Models
 {
-    public class PackList
+    public class PackItem
     {
-        
+        public int ID { get; }
+        public string Name {get; set;}
+        public string Size {get; set;}
+        public bool Packed {get; set;}
+
+        public static List<PackItem> PackList = new List<PackItem> { };
+        public static int idAssigner { get; set;}
+
+        public PackItem(string name, string size)
+        {
+            Name = name;
+            Size = size;
+            Packed = false;
+            idAssigner++;
+            ID = idAssigner;
+            PackList.Add(this);
+        }
+
+        public void CheckOffItem(int id)
+        {
+            for (int i=0; i<PackList.Count; i++ )
+            {
+                if (PackList[i].ID == id)
+                {
+                    PackList[i].Packed = true;
+                }
+            }
+        }
+
     }
 }
